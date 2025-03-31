6465084a53c8ca61009d51cc15b234e529284aec
AlroyYT <167989762+AlroyYT@users.noreply.github.com>
2025-03-31 23:30:18 +0530

    Update solution_functions.py

--- utils/solution_functions.py
+++ utils/solution_functions.py
@@ -310,7 +310,7 @@ def process_files_with_different_encodings(file_path=None):
 
 def use_github():
     # Change the return value based on your answer.
-    return "https://raw.githubusercontent.com/Sarthak-Sama/Temp-IIT-Assignment-Question/refs/heads/main/email.json"
+    return "https://raw.githubusercontent.com/AlroyYT/email-repo/refs/heads/main/email.json"
 
 
 def replace_across_files(file_path):
@@ -872,6 +872,7 @@ def host_your_portfolio_on_github_pages(email):
         "23f2001413@ds.study.iitm.ac.in": "https://debjeetsingha.github.io/", # Debjeet
         "23f1002942@ds.study.iitm.ac.in": "https://aman-v114.github.io/demo_repo/index.html", # Aman
         "21f3003062@ds.study.iitm.ac.in": "https://aditya-naidu.github.io/iit-githhubPages-testing/" # Aditya
+        "23f3001360@ds.study.iitm.ac.in": "https://alroyyt.github.io/" #Alroy
     }
     answer = urls[email]
     return answer
@@ -889,7 +890,7 @@ def use_google_colab(email):
         "23f1002942@ds.study.iitm.ac.in":"5aba1", # Aman
         "21f3003062@ds.study.iitm.ac.in": "518d1", # Aditya
         "23f2000942@ds.study.iitm.ac.in":"e70b4" # Aditi
-        
+        "23f3001360@ds.study.iitm.ac.in":"48b9f" #Alroy
     }
     answer = results[email]
     return answer
@@ -2028,7 +2029,7 @@ def create_a_scheduled_github_action(
     repo_owner="veershah1231",           
     repo_name="tdsGA4",               
     token={os.getenv("GITHUB_ACTION_TOKEN")},  # Set default to required token
-    email="23f3000709@ds.study.iitm.ac.in",  # Set default to required email
+    email="23f3001360@ds.study.iitm.ac.in",  # Set default to required email
     cron="30 2 * * *",                   
     workflow_name="daily-commit.yml"     
 ):
